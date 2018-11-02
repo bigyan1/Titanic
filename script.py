887b455c97efa1adc7bd675beac1b2f695a718b1
Bigyan <bigyan.kc1@gmail.com>
2018-11-02 15:33:08 -0400

    Modified code for feature processing

--- script.py
+++ script.py
@@ -1,13 +1,30 @@
 import pandas as pd
+#Read train and test csv into dataframe
 train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
 #Dropping columns that are not significant for decision making
 train_df = train_df.drop("Name", axis = 1)
 train_df = train_df.drop("Fare", axis = 1)
 train_df = train_df.drop("Ticket", axis = 1)
+#Similarly drop for test dataframe
+test_df = test_df.drop(["Name", "Fare", "Ticket"], axis = 1)
 #Save PassengerId column to use in the result
 passengerId = train_df["PassengerId"]
 train_df = train_df.drop("PassengerId", axis = 1)
+#Save passengerId for test dataframe
+passengerId_test = test_df["PassengerId"]
+test_df = test_df.drop("PassengerId", axis = 1)
 #Fill Missing values in Age and Embarked columns with mean and mode values
 train_df["Age"].fillna(train_df["Age"].mean(), inplace = True)
 train_df["Embarked"].fillna(train_df["Embarked"].mode()[0], inplace = True)
+#Replace 'male' with 1 and 'female' with 0 in Sex column
+train_df['Sex'].replace("male", 1, inplace = True)
+train_df['Sex'].replace("female", 1, inplace = True)
+#Split the features column and 'Survived' column
+train_Y = train_df["Survived"]
+train_X = train_df.drop("Survived", axis =1)
+#Converting categorical value in 'Embarked' column into a new column
+train_X_onehot = pd.get_dummies(train_X, columns = ["Embarked"])
+test_onehot = pd.get_dummies(test_df, columns = ["Embarked"])
+
 
